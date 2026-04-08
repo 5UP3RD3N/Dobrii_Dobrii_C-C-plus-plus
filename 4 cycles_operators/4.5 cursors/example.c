32e90c76fe8872a07d4a6274149c2ebced3c6848
3dendroid <denkingbad@gmail.com>
2026-04-08 10:30:30 +0200

    NEXT: feat_1.c

--- 4 cycles_operators/4.5 cursors/example.c
+++ 4 cycles_operators/4.5 cursors/example.c
@@ -1,1 +1,17 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    int d = 10;
+    int *gpt = &d;
+
+    // gpt = &d;
+
+    printf("gpt = %p, *gpt = %d, d = %d\n, gpt, *gpt, d");
+
+    *gpt = 75432;
+
+    printf("gpt = %p, *gpt = %d, d = %d\n, gpt, *gpt, d");
+
+    return 0;
+}
