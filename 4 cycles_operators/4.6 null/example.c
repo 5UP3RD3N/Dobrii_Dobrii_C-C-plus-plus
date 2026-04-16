5f457a0e87ec5b64a6505025f7771db3fa5c85fd
3dendroid <denkingbad@gmail.com>
2026-04-16 11:56:50 +0200

    NEXT: feat_3.c

--- 4 cycles_operators/4.6 null/example.c
+++ 4 cycles_operators/4.6 null/example.c
@@ -1,1 +1,17 @@
-# NEXT
+#include <stdio.h>
+
+int main(void)
+{
+    int arg = 777;
+    int *ptr_arg
+    char *ptr;
+
+    ptr_arg = &arg;
+    ptr = ptr_arg;
+
+    *ptr = 1;
+    
+
+    return 0;
+
+}
