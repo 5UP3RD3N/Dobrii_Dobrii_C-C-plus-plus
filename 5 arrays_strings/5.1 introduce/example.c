5ff3a7569afcdc48139ae1744f3083aece8beb86
3dendroid <denkingbad@gmail.com>
2026-05-04 09:44:15 +0200

    NEXT: feat_1.c

--- 5 arrays_strings/5.1 introduce/example.c
+++ 5 arrays_strings/5.1 introduce/example.c
@@ -1,1 +1,17 @@
-# NEXT
+#include <stdio.h>
+
+#define TOTAL_MARKS 13
+
+int main(void)
+{
+    int marks[TOTAL_MARKS];
+
+    marks[2] = 4;
+    marks[0] = 2 * 3;
+    marks[12] = 7 - 2;
+
+    for (int i = 0; i < TOTAL_MARKS; ++i)
+        printf("%d ", marks[i]);
+
+    return 0;
+}
